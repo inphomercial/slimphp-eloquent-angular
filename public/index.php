7ddc665baa306b0be71eba85a40b1e696cee2a52
Sean Brown <inphomercial@gmail.com>
2014-09-12 16:07:14 -0600

    Cleaning up index

--- public/index.php
+++ public/index.php
@@ -2,8 +2,17 @@
 <html lang="en" ng-app="TestApp">
 <head>
     <title>Test App</title>
-
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css">
     <link rel="stylesheet" type="text/css" href="frontend/css/main.css">
+
+    <!-- Optional theme -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap-theme.min.css">
+
+    <!-- JQuery CDN -->
+    <script src="//code.jquery.com/jquery-1.11.0.min.js"></script>
+
+    <!-- Latest compiled and minified JavaScript -->
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
 
     <meta name="viewport" content="width=device-width, initial-scale=1">
 </head>
@@ -18,43 +27,35 @@
         </div>
         <div class="collapse navbar-collapse">
             <ul class="nav navbar-nav">
+		<li><a href="#main"><i class="fa fa-home"></i> Main</a></li>
                 <li><a href="#users"><i class="fa fa-home"></i> Users </a></li>
-                <li><a href="#main"><i class="fa fa-home"></i> Main</a></li>
             </ul>
         </div>
     </div>
 
-    <br><br>
+    <br><br><br>
 
-    <!-- All of our Angular views go here /public/partials/* -->
-    <div class="container" class="row">
-        <div class="col-xs-12">
-	       <div ng-view></div>
+    <div class=="row">
+	<!-- All of our Angular views go here /public/partials/* -->
+	<div class="container" class="row">
+	    <div class="col-xs-12">
+	        <div ng-view></div>
+	    </div>
         </div>
     </div>
 
-	<!-- <div class="container" ng&#45;controller="TestController"> -->
-	<!-- 	<span>{{ book }}</span> -->
-	<!-- </div> -->
-
-    <br><br>
-
     <!-- Source Venders -->
     <script type="text/javascript" src="frontend/js/vendors/angular.min.js"></script>
-    <!-- <script type="text/javascript" src="/js/vendors/jquery&#45;2&#45;1&#45;0.min.js"></script> -->
     <script type="text/javascript" src="frontend/js/vendors/angular-route.min.js"></script>
 
     <!-- Source TestApp -->
     <script type="text/javascript" src="frontend/js/apps/test.js"></script>
 
     <!-- Source Services -->
-    <!-- <script type="text/javascript" src="/js/services/authentication_service.js"></script> -->
-    <!-- <script type="text/javascript" src="/js/services/session_service.js"></script> -->
-    <!-- <script type="text/javascript" src="/js/services/book_service.js"></script> -->
+    <!-- <script type="text/javascript" src="frontend/js/services/test.js"></script> -->
 
     <!-- Source Controllers -->
     <script type="text/javascript" src="frontend/js/controllers/test_controller.js"></script>
-    <!-- <script type="text/javascript" src="/js/controllers/books_controller.js"></script> -->
 
     <!-- Source Directives -->
 
